1bdf34e081d6bcb93e485aec89cc269e93ce1018
alexandersamuelbrown <alicks.b@gmail.co>
2019-08-25 13:37:10 -0700

    Addition of the move manager a class that contains the move history of the game.

---
+++ venv/lib/move.py
@@ -0,0 +1,23 @@
+
+
+class Move:
+    piece = None
+    from_x, from_y, to_x, to_y = (0, 0, 0, 0)
+
+    def __init__(self, piece, from_x, from_y, to_x, to_y):
+        self.from_x, self.from_y, self.to_x, self.to_y  = (from_x, from_y, to_x, to_y)
+        self.piece = piece
+
+class MoveManager:
+    moves = []
+    board = None
+    pieces = None
+
+    def make_move(self, piece, to_x, to_y):
+        pass
+
+    def get_last(self):
+        pass
+
+    def undo(self):
+        pass
